86dbf131bed89a80a0d6dfbe42c0742ae9605f67
James McCollum <jmccollum@signet-studios.com>
2016-10-22 15:07:31 +0000

    Begin writing unit tests

--- spec/unoSpec.js
+++ spec/unoSpec.js
@@ -1,9 +1,71 @@
-/*global expect*/
+/* global expect */
+/* global spyOn */
+/* global jasmine */
 var uno = require('../lib/uno.js');
 
 describe('unoGame', function(){
-    describe('announceTurn', function(){
+    beforeEach(function(){
+        this.storage = {
+            channels: {
+                getAsync: jasmine.createSpy('getAsync'),
+                saveAsync: jasmine.createSpy('saveAsync')
+            }
+        };
+        this.sendMessage = jasmine.createSpy('sendMessage');
+        this.config = {
+            storage: this.storage,
+            sendMessage: this.sendMessage
+        };
         
+        this.uno = uno(this.config);
+        this.message = {};
+        this.game = {
+            currentCard:{
+                color: 'red',
+                value: '8'
+            },
+            turnOrder: ['player1', 'player2']
+        };
+    });
+    
+    describe('announceTurn', function(){
+        it('should do nothing if there is no game', function(){
+            //Arrange
+            
+            //Act
+            this.uno.announceTurn(this.message, undefined);
+            
+            //Assert
+            expect(this.sendMessage).not.toHaveBeenCalled();
+        });
+        
+        it('should announce the current up card', function(){
+            //Arrange
+            
+            //Act
+            this.uno.announceTurn(this.message, this.game);
+            
+            //Assert
+            expect(this.sendMessage.calls.length).toBeGreaterThan(0);
+            expect(this.sendMessage).toHaveBeenCalledWith(this.message, {
+                text: 'The current up card is:',
+                attachments: [{
+                    color: '#ff0000',
+                    text: 'red 8'
+                }]
+            });
+        });
+        
+        it('should announce the current player', function(){
+            //Arrange
+            
+            //Act
+            this.uno.announceTurn(this.message, this.game);
+            
+            //Assert
+            expect(this.sendMessage.calls.length).toBeGreaterThan(0);
+            expect(this.sendMessage).toHaveBeenCalledWith(this.message, 'It is player1\'s turn.\nType `/uno play [card]`, `/uno draw` or `/uno status` to begin your turn.');
+        });
     });
     
     describe('beginGame', function(){
