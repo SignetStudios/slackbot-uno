20d013b6d9cc5d38aa012205ad4f231c36ae85c6
James McCollum <jmccollum@signet-studios.com>
2016-10-22 13:23:09 +0000

    Removed debug messages

--- slack_bot.js
+++ slack_bot.js
@@ -33,7 +33,6 @@ var unoGame = require('./lib/uno.js')({
 //TODO: Allow for commands via @mentions as well
 
 slapp.command('/uno', '^new$', (msg) => {
-    console.log('NEW');
     unoGame.getGame(msg, true).then(function(game){
         unoGame.initializeGame(msg, game);
     });
@@ -42,14 +41,12 @@ slapp.command('/uno', '^new$', (msg) => {
 //The following should hear most combinations of cards that can be played
 //TODO: Consider breaking these out into seperate functions for easier debugging
 slapp.command('/uno', '^play(?: (r(?:ed)?|y(?:ellow)?|g(?:reen)?|b(?:lue)?|w(?:ild)?|d(?:raw ?4)?)(?: ?([1-9]|s(?:kip)?|r(?:everse)?|d(?:(?:raw ?)?2?)?))?)?$', (msg, text, color, value) => {
-    console.log('PLAY');
     unoGame.getGame(msg).then(function(game){
         unoGame.playCard(msg, game, color, value);
     });
 });
 
 slapp.command('/uno', '^color (r(?:ed)?|y(?:ellow)?|g(?:reen)?|b(?:lue)?)', (msg, text, color) => {
-    console.log('COLOR');
     unoGame.getGame(msg).then(function(game){
         unoGame.setWildColor(msg, game, color);
     });
@@ -57,14 +54,12 @@ slapp.command('/uno', '^color (r(?:ed)?|y(?:ellow)?|g(?:reen)?|b(?:lue)?)', (msg
 
 //TODO: Remove when done testing (or not)
 slapp.command('/uno', '^reset thisisthepassword$', (msg) => {
-    console.log('RESET');
     unoGame.getGame(msg, true).then(function(game){
         unoGame.resetGame(msg, game);
     });
 });
 
 slapp.command('/uno', '^setup', (msg) => {
-    console.log('SETUP');
     unoGame.getGame(msg).then(function(game){
         for (var i = 2; i <= 2; i++){
             var mockUser = 'Player' + i;
@@ -74,21 +69,18 @@ slapp.command('/uno', '^setup', (msg) => {
 });
 
 slapp.command('/uno', '^join', (msg) => {
-    console.log('JOIN');
     unoGame.getGame(msg).then(function(game){
         unoGame.joinGame(msg, game);
     });
 });
 
 slapp.command('/uno', '^quit', (msg) => {
-    console.log('QUIT');
     unoGame.getGame(msg).then(function(game){
         unoGame.quitGame(msg, game);
     });
 });
 
 slapp.command('/uno', '^status', (m) => {
-    console.log('STATUS');
     unoGame.getGame(m).then(function(g){
         unoGame.reportHand(m, g);
         unoGame.reportTurnOrder(m, g, true);
@@ -97,14 +89,12 @@ slapp.command('/uno', '^status', (m) => {
 });
 
 slapp.command('/uno', '^start', (m) => {
-    console.log('START');
     unoGame.getGame(m).then(function(g){
         unoGame.beginGame(m, g);
     });
 });
 
 slapp.command('/uno', '^draw', (m) => {
-    console.log('DRAW');
     unoGame.getGame(m).then(function(g){
         unoGame.drawCard(m, g);
     });
