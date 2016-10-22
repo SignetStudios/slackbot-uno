e16b2962fc9f3382dc629eda458b2538f4eb2938
James McCollum <jmccollum@signet-studios.com>
2016-10-22 14:17:57 +0000

    Framework for unit tests

--- spec/unoSpec.js
+++ spec/unoSpec.js
@@ -1,1 +1,57 @@
+/*global expect*/
 var uno = require('../lib/uno.js');
+
+describe('unoGame', function(){
+    describe('announceTurn', function(){
+        
+    });
+    
+    describe('beginGame', function(){
+        
+    });
+    
+    describe('drawCard', function(){
+        
+    });
+    
+    describe('getGame', function() {
+        
+    });
+    
+    describe('initializeGame', function() {
+        
+    });
+    
+    describe('joinGame', function() {
+        
+    });
+    
+    describe('playCard', function() {
+        
+    });
+    
+    describe('quitGame', function() {
+        
+    });
+    
+    describe('reportHand', function() {
+        
+    });
+    
+    describe('reportScores', function() {
+        
+    });
+    
+    describe('reportTurnOrder', function() {
+        
+    });
+    
+    describe('resetGame', function() {
+        
+    });
+    
+    describe('setWildColor', function() {
+        
+    });
+    
+});
