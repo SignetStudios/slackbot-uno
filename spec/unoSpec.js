1b43ea3e43aa0fbf2854adab2ed59534c15afd0e
James McCollum <jmccollum@signet-studios.com>
2016-10-22 17:49:13 +0000

    Add unit tests for beginGame

--- spec/unoSpec.js
+++ spec/unoSpec.js
@@ -2,29 +2,45 @@
 /* global spyOn */
 /* global jasmine */
 var uno = require('../lib/uno.js');
+var nock = require('nock');
+var Promise = require('bluebird');
 
 describe('unoGame', function(){
     beforeEach(function(){
+        this.promise = require('bluebird');
         this.storage = {
-            channels: {
-                getAsync: jasmine.createSpy('getAsync'),
-                saveAsync: jasmine.createSpy('saveAsync')
-            }
+            channels: Promise.promisifyAll({
+                get: jasmine.createSpy('get').andCallFake(function(obj, cb){ cb(); }),
+                save: jasmine.createSpy('save').andCallFake(function(obj, cb){ cb(); })
+            })
         };
         this.sendMessage = jasmine.createSpy('sendMessage');
         this.config = {
             storage: this.storage,
-            sendMessage: this.sendMessage
+            sendMessage: this.sendMessage,
+            Promise: this.promise
         };
         
         this.uno = uno(this.config);
-        this.message = {};
+        this.message = {
+            body: {}
+        };
         this.game = {
+            id: 'gameId',
             currentCard:{
                 color: 'red',
                 value: '8'
             },
-            turnOrder: ['player1', 'player2']
+            turnOrder: ['player1', 'player2'],
+            player1: 'player1',
+            players: {
+                player1: {
+                    hand: []
+                },
+                player2: {
+                    hand: []
+                }
+            }
         };
     });
     
@@ -69,7 +85,154 @@ describe('unoGame', function(){
     });
     
     describe('beginGame', function(){
+        beforeEach(function(){
+            this.httpScope = nock('http://deckofcardsapi.com')
+                .get('/api/deck/deckid').reply(200)
+                .get('/api/deck/deckid').reply(200)
+                .get('/api/deck/new/shuffle/').query({deck_count: '2'}).reply(200, {success: true, deck_id: "deckid", remaining: 104})
+                .get('/api/deck/deckid/draw/').query({count: '1'}).reply(200, {success: true, cards: [{suit: "DIAMONDS", value: "ACE", code: "AD"}]})
+                .get('/api/deck/deckid/draw/').query({count: '7'}).reply(200, {success: true, remaining: 100, cards: [{suit: "DIAMONDS", value: "ACE", code: "AD"},{suit: "DIAMONDS", value: "ACE", code: "AD"},{suit: "DIAMONDS", value: "ACE", code: "AD"},{suit: "DIAMONDS", value: "ACE", code: "AD"},{suit: "DIAMONDS", value: "ACE", code: "AD"},{suit: "DIAMONDS", value: "ACE", code: "AD"},{suit: "DIAMONDS", value: "ACE", code: "AD"}]})
+                .get('/api/deck/deckid/draw/').query({count: '7'}).reply(200, {success: true, remaining: 100, cards: [{suit: "DIAMONDS", value: "ACE", code: "AD"},{suit: "DIAMONDS", value: "ACE", code: "AD"},{suit: "DIAMONDS", value: "ACE", code: "AD"},{suit: "DIAMONDS", value: "ACE", code: "AD"},{suit: "DIAMONDS", value: "ACE", code: "AD"},{suit: "DIAMONDS", value: "ACE", code: "AD"},{suit: "DIAMONDS", value: "ACE", code: "AD"}]});
+        });
         
+        it('should do nothing if there is no game', function(){
+            //Arrange
+            this.originalGame = JSON.parse(JSON.stringify(this.game));
+            
+            //Act
+            this.uno.beginGame(this.message, undefined);
+            
+            //Assert
+            expect(this.sendMessage).not.toHaveBeenCalled();
+            expect(this.game).toEqual(this.originalGame);
+        });
+        
+        it('should send a message and do nothing if the player is not player 1', function(){
+             //Arrange
+             this.originalGame = JSON.parse(JSON.stringify(this.game));
+             this.message.body.user_name = 'player2';
+             
+             //Act
+             this.uno.beginGame(this.message, this.game);
+             
+             //Assert
+             expect(this.sendMessage).toHaveBeenCalledWith(this.message, 'Only player 1 (player1) can start the game.', true);
+             expect(this.game).toEqual(this.originalGame);
+        });
+        
+        it('should send a message and do nothing if there are less than 2 players', function(){
+             //Arrange
+             delete this.game.players.player2;
+
+             this.originalGame = JSON.parse(JSON.stringify(this.game));
+             this.message.body.user_name = 'player1';
+
+             //Act
+             this.uno.beginGame(this.message, this.game);
+             
+             //Assert
+             expect(this.sendMessage).toHaveBeenCalledWith(this.message, 'You need at least two players to begin playing.', true);
+             expect(this.game).toEqual(this.originalGame);
+        });
+        
+        it('should send a message and do nothing if the game is already started', function(){
+             //Arrange
+             spyOn(this.uno, 'reportTurnOrder');
+             this.game.started = true;
+
+             this.originalGame = JSON.parse(JSON.stringify(this.game));
+             this.message.body.user_name = 'player1';
+
+             //Act
+             this.uno.beginGame(this.message, this.game);
+             
+             //Assert
+             expect(this.sendMessage).toHaveBeenCalledWith(this.message, 'The game is already started.', true);
+             expect(this.game).toEqual(this.originalGame);
+        });
+        
+        it('should report the current turn order if the game is already started', function(){
+             //Arrange
+             spyOn(this.uno, 'reportTurnOrder');
+             this.game.started = true;
+
+             this.originalGame = JSON.parse(JSON.stringify(this.game));
+             this.message.body.user_name = 'player1';
+
+             //Act
+             this.uno.beginGame(this.message, this.game);
+             
+             //Assert
+             expect(this.uno.reportTurnOrder).toHaveBeenCalledWith(this.message, this.game, true);
+             expect(this.game).toEqual(this.originalGame);
+        });
+        
+        it('marks the game as started', function(done){
+            //Arrange
+            this.message.body.user_name = 'player1';
+            
+            
+            //Act
+            this.uno.beginGame(this.message, this.game).then(done());
+            
+            //Assert
+            expect(this.game.started).toEqual(true);
+        });
+        
+        it('broadcasts a message that the game has started', function(done){
+            //Arrange
+            this.message.body.user_name = 'player1';
+            
+            
+            //Act
+            this.uno.beginGame(this.message, this.game).then(done());
+            
+            //Assert
+            expect(this.sendMessage).toHaveBeenCalledWith(this.message, 'Game has started! Shuffling the deck and dealing the hands.');
+        });
+        
+        it('gives each player 7 cards', function(done){
+            //Arrange
+            this.message.body.user_name = 'player1';
+            var self = this;
+            
+            //Act
+            this.uno.beginGame(this.message, this.game).then(function(){
+                //Assert
+                for (var player in self.game.players){
+                    expect(self.game.players[player].hand.length).toEqual(7);
+                }
+                done();
+            });
+        });
+        
+        it('announces the turn information', function(done){
+            //Arrange
+            this.message.body.user_name = 'player1';
+            spyOn(this.uno, 'announceTurn');
+            var self = this;
+            
+            //Act
+            this.uno.beginGame(this.message, this.game).then(function(){
+                //Assert
+                expect(self.uno.announceTurn).toHaveBeenCalledWith(self.message, self.game);
+                done();
+            });
+        });
+        
+        it('tells player1 their hand', function(done){
+            //Arrange
+            this.message.body.user_name = 'player1';
+            spyOn(this.uno, 'reportHand');
+            var self = this;
+            
+            //Act
+            this.uno.beginGame(this.message, this.game).then(function(){
+                //Assert
+                expect(self.uno.reportHand).toHaveBeenCalledWith(self.message, self.game);
+                done();
+            });
+        });
     });
     
     describe('drawCard', function(){
